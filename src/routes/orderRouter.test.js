cce565b540c4e49056f67160f4d32e414da3131e
blasson42 <brycelasson@gmail.com>
2025-09-30 14:54:36 -0600

    Updated menu test

--- src/routes/orderRouter.test.js
+++ src/routes/orderRouter.test.js
@@ -34,15 +34,16 @@ beforeAll(async () => {
 test('get pizza menu', async () => {
     const res = await request(app).get('/api/order/menu');
     expect(res.status).toBe(200);
-    expect(res.body.length).toBeGreaterThan(0);
-    expect(Object.keys(res.body[0]).sort()).toEqual(['id', 'title', 'image', 'price', 'description'].sort());
-    expect(res.body[0]).toEqual({
-        id: expect.anything(),
-        title: expect.anything(),
-        image: expect.anything(),
-        price: expect.anything(),
-        description: expect.anything()
-    });
+    if (res.body.length !== 0) {
+        expect(Object.keys(res.body[0]).sort()).toEqual(['id', 'title', 'image', 'price', 'description'].sort());
+        expect(res.body[0]).toEqual({
+            id: expect.anything(),
+            title: expect.anything(),
+            image: expect.anything(),
+            price: expect.anything(),
+            description: expect.anything()
+        });
+    }
 });
 
 test('add menu item without permission', async () => {
