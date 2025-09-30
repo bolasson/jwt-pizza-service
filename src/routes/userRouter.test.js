886458dd301b6438cc6119a64dff7333b42778d5
blasson42 <brycelasson@gmail.com>
2025-09-29 21:29:49 -0600

    Implemented updateUser test

--- src/routes/userRouter.test.js
+++ src/routes/userRouter.test.js
@@ -3,6 +3,7 @@ const app = require('../service');
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
+let userId;
 
 function randomName() {
     return Math.random().toString(36).substring(2, 12);
@@ -16,6 +17,7 @@ beforeAll(async () => {
     testUser.email = randomName() + '@test.com';
     const registerRes = await request(app).post('/api/auth').send(testUser);
     testUserAuthToken = registerRes.body.token;
+    userId = registerRes.body.user.id;
     expectValidJwt(testUserAuthToken);
 });
 
@@ -24,4 +26,14 @@ test('get authenticated user', async () => {
     expect(res.status).toBe(200);
     expect(res.body).toMatchObject({ name: testUser.name, email: testUser.email, roles: [{ role: 'diner' }] });
     expect(res.body).toHaveProperty('id');
+});
+
+test('update user', async () => {
+    const newName = 'papa john totino murphy  ' + randomName();
+    const newEmail = randomName() + '@test.com';
+    const res = await request(app).put(`/api/user/${userId}`).send({ name: newName, email: newEmail, password: 'a' }).set('Authorization', `Bearer ${testUserAuthToken}`);
+    expect(res.status).toBe(200);
+    expectValidJwt(res.body.token);
+    expect(res.body.user).toMatchObject({ name: newName, email: newEmail, roles: [{ role: 'diner' }] });
+    expect(res.body.user).toHaveProperty('id');
 });
