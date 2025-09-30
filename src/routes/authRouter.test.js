68e74523e90bc37e09a8426410a75470daf65bfa
blasson42 <brycelasson@gmail.com>
2025-09-29 19:41:05 -0600

    Base authRouter tests

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -18,11 +18,29 @@ async function createAdminUser() {
     return { ...user, password: 'automateAUTOMATEaUtOmAtE!' };
 }
 
+function expectValidJwt(potentialJwt) {
+    expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+}
+
 beforeAll(async () => {
     testUser.email = randomName() + '@test.com';
     const registerRes = await request(app).post('/api/auth').send(testUser);
     testUserAuthToken = registerRes.body.token;
     expectValidJwt(testUserAuthToken);
+});
+
+test('register with missing fields', async () => {
+    const res = await request(app).post('/api/auth').send({ name: 'a', email: 'b' });
+    expect(res.status).toBe(400);
+    expect(res.body).toEqual({ message: 'name, email, and password are required' });
+
+    const res2 = await request(app).post('/api/auth').send({ name: 'a', password: 'b' });
+    expect(res2.status).toBe(400);
+    expect(res2.body).toEqual({ message: 'name, email, and password are required' });
+
+    const res3 = await request(app).post('/api/auth').send({ email: 'a', password: 'b' });
+    expect(res3.status).toBe(400);
+    expect(res3.body).toEqual({ message: 'name, email, and password are required' });
 });
 
 test('login', async () => {
@@ -35,6 +53,8 @@ test('login', async () => {
     expect(loginRes.body.user).toMatchObject(expectedUser);
 });
 
-function expectValidJwt(potentialJwt) {
-    expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
-}
+test('logout', async () => {
+    const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`);
+    expect(logoutRes.status).toBe(200);
+    expect(logoutRes.body).toEqual({ message: 'logout successful' });
+});
