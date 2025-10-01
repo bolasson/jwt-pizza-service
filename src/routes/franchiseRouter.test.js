99fca95edd33c7fce231461a810ef6ecc46d3dbf
blasson42 <brycelasson@gmail.com>
2025-09-30 22:22:23 -0600

    added get user franchises test

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -6,6 +6,8 @@ const testUser = { name: 'test diner', email: 'reg@test.com', password: 'a' };
 const testFranchise = { "name": "test franchise", "admins": [{ "email": "f@jwt.com" }] };
 
 let testUserAuthToken;
+let testAdminAuthToken;
+let testAdminUser;
 
 function randomName() {
     return Math.random().toString(36).substring(2, 12);
@@ -33,16 +35,25 @@ beforeAll(async () => {
     const loginAdminRes = await request(app).put('/api/auth').send({ email: adminUser.email, password: 'toomanysecretsformetoremember' });
     expectValidJwt(loginAdminRes.body.token);
 
-    const adminAuthToken = loginAdminRes.body.token;
+    testAdminUser = loginAdminRes.body.user;
+
+    testAdminAuthToken = loginAdminRes.body.token;
     testFranchise.name = randomName();
     testFranchise.admins[0].email = adminUser.email;
-    await request(app).post('/api/franchise').set('Authorization', `Bearer ${adminAuthToken}`).send(testFranchise);
+    await request(app).post('/api/franchise').set('Authorization', `Bearer ${testAdminAuthToken}`).send(testFranchise);
 });
 
-test('should list all franchises with correct response shape', async () => {
+test('get all franchises', async () => {
     const res = await request(app).get('/api/franchise').set('Authorization', `Bearer ${testUserAuthToken}`);
     expect(res.status).toBe(200);
     expect(res.body).toHaveProperty('franchises');
     expect(res.body.franchises.map(f => f.name)).toContain(testFranchise.name);
     expect(Array.isArray(res.body.franchises)).toBe(true);
+});
+
+test('get user franchises', async () => {
+    const res = await request(app).get(`/api/franchise/${testAdminUser.id}`).set('Authorization', `Bearer ${testAdminAuthToken}`);
+    expect(res.status).toBe(200);
+    expect(res.body.map(f => f.name)).toContain(testFranchise.name);
+    expect(res.body.map(f => f.admins).flat().map(a => a.email)).toContain(testFranchise.admins[0].email);
 });
